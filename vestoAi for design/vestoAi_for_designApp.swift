cfbeb45b6aca386190a01737f1c012fe83d8e267
iamkane22 <manaflikenan222@gmail.com>
2026-03-09 03:59:38 +0400

    Initial Commit

---
+++ vestoAi for design/vestoAi_for_designApp.swift
@@ -0,0 +1,17 @@
+//
+//  vestoAi_for_designApp.swift
+//  vestoAi for design
+//
+//  Created by Kenan on 09.03.26.
+//
+
+import SwiftUI
+
+@main
+struct vestoAi_for_designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
